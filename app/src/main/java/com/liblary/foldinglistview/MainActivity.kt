bfe111ce3a0a6b85414961e5df03cb083bcffc51
CHJUNE-android <chjune0205@gmail.com>
2019-12-06 11:39:09 +0900

    ViewItem부분 공부중 equals 재 정의

--- app/src/main/java/com/liblary/foldinglistview/MainActivity.kt
+++ app/src/main/java/com/liblary/foldinglistview/MainActivity.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.liblary.foldinglistview.fold.FoldingCell
+import com.liblary.foldinglistview.fold.FoldingCellListAdapter
+import com.liblary.foldinglistview.fold.ViewItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
     private fun init(){
         // prepare elements to display
-        val items = Item.testingList
+        val items = ViewItem.testingList
         //아이템 역할 -
 
         // add custom btn handler to first list item
@@ -28,7 +31,10 @@ class MainActivity : AppCompatActivity() {
         ).show() }
 
         // create custom adapter that holds elements and their state (we need hold a id's of unfolded elements for reusable elements)
-        val adapter = FoldingCellListAdapter(this, items as List<ClipData.Item>)
+        val adapter = FoldingCellListAdapter(
+            this,
+            items as List<ClipData.Item>
+        )
 
         // add default btn handler for each request btn on each item if custom handler not found
         adapter.defaultRequestBtnClickListener = View.OnClickListener {
